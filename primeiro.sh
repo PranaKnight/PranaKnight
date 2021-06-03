7742576507fc376b344eed2e28ad15944838df7b
PranaKnight <pranaknight@gmail.com>
2021-06-03 02:29:48 -0300

    Testando 1, 2, 3?

---
+++ primeiro.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# primeiro.sh - Nosso primeiro progrmaa em shell
+#
+# Homepage: http://www.utah.com.br
+# Autor: Filipe <fgrahl2010@gmail.com>
+#Mantenedor: UTAH
+#
+# ----------------------------------------------------
+#
+# Este progrmaa ira mostar a mensagem "MEu Primeipro programa em shelL" na tela ao ser executado.
+#
+#Exemplo de execução:
+#
+# $ ./primeir.sh
+# Meu primeiro Progrmaa em Shell
+#
+# Historico de Versões
+#
+#Versão: 1.0
+#
+# COPYRIGHT: ESte programa é GPL
+
+echo "Meu Primeiro Programa em Shell"
+
+
